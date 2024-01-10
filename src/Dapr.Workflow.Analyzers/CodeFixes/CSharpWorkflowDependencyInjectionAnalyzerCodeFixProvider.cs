075fd624c6d8f5d962e10497d495d817209ffd3f
Whit Waldo <whit.waldo@innovian.net>
2024-01-09 18:48:15 -0600

    WIP code fixes for both dependency injection validators + tests
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ src/Dapr.Workflow.Analyzers/CodeFixes/CSharpWorkflowDependencyInjectionAnalyzerCodeFixProvider.cs
@@ -0,0 +1,115 @@
+﻿// using System.Collections.Immutable;
+// using System.Linq;
+// using System.Threading;
+// using System.Threading.Tasks;
+// using Microsoft.CodeAnalysis;
+// using Microsoft.CodeAnalysis.CodeActions;
+// using Microsoft.CodeAnalysis.CodeFixes;
+// using Microsoft.CodeAnalysis.CSharp;
+// using Microsoft.CodeAnalysis.CSharp.Syntax;
+// using Microsoft.CodeAnalysis.Editing;
+// using Document = Microsoft.CodeAnalysis.Document;
+//
+// namespace Dapr.Workflow.Analyzers.CodeFixes;
+//
+// /// <summary>
+// /// Implements a Roslyn code fix provider that adds a registration for each type that implements `IWorkflow`
+// /// but which isn't also registered within the `AddDaprWorkflow` dependency injection registration. 
+// /// </summary>
+// [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(CSharpWorkflowDependencyInjectionAnalyzerCodeFixProvider))]
+// public sealed class CSharpWorkflowDependencyInjectionAnalyzerCodeFixProvider : CodeFixProvider
+// {
+// 	//Specify the diagnostic IDs of analyzers that are expected to be linked
+//     /// <inheritdoc/>
+// 	public sealed override ImmutableArray<string> FixableDiagnosticIds =>
+// 		ImmutableArray.Create(CSharpWorkflowDependencyInjectionValidationAnalyzer.DiagnosticId);
+//
+//     /// <inheritdoc/>
+// 	public override FixAllProvider? GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+//
+// 	/// <summary>
+// 	/// Computes one or more fixes for the specified <see cref="T:Microsoft.CodeAnalysis.CodeFixes.CodeFixContext" />.
+// 	/// </summary>
+// 	/// <param name="context">
+// 	/// A <see cref="T:Microsoft.CodeAnalysis.CodeFixes.CodeFixContext" /> containing context information about the diagnostics to fix.
+// 	/// The context must only contain diagnostics with a <see cref="P:Microsoft.CodeAnalysis.Diagnostic.Id" /> included in the <see cref="P:Microsoft.CodeAnalysis.CodeFixes.CodeFixProvider.FixableDiagnosticIds" /> for the current provider.
+// 	/// </param>
+// 	public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+// 	{
+// 		var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+//
+// 		var diagnostic = context.Diagnostics.First();
+// 		var diagnosticSpan = diagnostic.Location.SourceSpan;
+// 		
+// 		//Find the type declaration identified by the diagnostic
+// 		var declaration = root?.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf().OfType<TypeDeclarationSyntax>()
+// 			.FirstOrDefault();
+// 		
+// 		//Register a code action that will invoke the fix
+// 		if (declaration is not null)
+// 		{
+// 			context.RegisterCodeFix(
+// 				CodeAction.Create(title: Resources.WF0001CodeFixTitle,
+// 					createChangedDocument: c => RegisterWorkflowAsync(context.Document, declaration, c),
+// 					equivalenceKey: Resources.WF0001CodeFixTitle), diagnostic);
+// 		}
+// 	}
+//
+// 	private async Task<Document> RegisterWorkflowAsync(Document document, TypeDeclarationSyntax typeDecl,
+// 		CancellationToken cancellationToken)
+// 	{
+// 		//Get the semantic model and symbol of the type declaration
+// 		var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+//
+// 		if (semanticModel is null)
+// 			return document;
+//
+// 		if (ModelExtensions.GetDeclaredSymbol(semanticModel, typeDecl, cancellationToken) is not INamedTypeSymbol typeSymbol)
+// 			return document;
+// 		
+// 		//Get the well-known type (IWorkflow)
+// 		var workflowType = semanticModel.Compilation.GetTypeByMetadataName("IWorkflow");
+// 		
+// 		//Check if the type implements IWorkflow
+// 		var interfaceType =
+// 			typeSymbol.AllInterfaces.FirstOrDefault(i => SymbolEqualityComparer.Default.Equals(i, workflowType));
+// 		if (interfaceType is null)
+// 			return document;
+// 		
+// 		//Get the name of the registration method
+// 		const string methodName = "RegisterWorkflow";
+//
+// 		//Create a document editor
+// 		var editor = await DocumentEditor.CreateAsync(document, cancellationToken);
+// 		
+// 		//Find the lambda expression that configures the workflow options
+// 		var lambdaExpr = editor.OriginalRoot.DescendantNodes().OfType<SimpleLambdaExpressionSyntax>()
+// 			.FirstOrDefault(l => l.Parameter.Identifier.ValueText == "options");
+// 		if (lambdaExpr is null)
+// 			return document;
+// 		
+// 		//Find the block of the lambda expression
+// 		if (lambdaExpr.Body is not BlockSyntax block)
+// 		{
+// 			return document;
+// 		}
+// 		
+// 		//Create a statement that invokes the registration method with the type argument
+// 		var statement = SyntaxFactory.ExpressionStatement(
+// 			SyntaxFactory.InvocationExpression(
+// 				SyntaxFactory.MemberAccessExpression(
+// 					SyntaxKind.SimpleMemberAccessExpression,
+// 					SyntaxFactory.IdentifierName("options"),
+// 					SyntaxFactory.GenericName(
+// 						SyntaxFactory.Identifier(methodName),
+// 						SyntaxFactory.TypeArgumentList(
+// 							SyntaxFactory.SingletonSeparatedList<TypeSyntax>(
+// 								SyntaxFactory.IdentifierName(typeSymbol.Name)))))));
+// 		
+// 		//Add the statement to the end of the block
+// 		editor.ReplaceNode(block, block.AddStatements(statement));
+// 		
+// 		//Return the updated document
+// 		return editor.GetChangedDocument();
+// 	}
+// }
---
+++ test/Dapr.Workflow.Analyzers.Test/CodeFixes/WorkflowDependencyInjectionValidationCodeFixTests.cs
@@ -0,0 +1,391 @@
+﻿// using System.Collections.Immutable;
+// using Microsoft.CodeAnalysis;
+// using Microsoft.CodeAnalysis.Testing;
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+//
+// namespace Dapr.Workflow.Analyzers.Test;
+//
+// using VerifyCS = Tests.MsTest.Verifiers.CSharpCodeFixVerifier<
+//     Analyzers.CSharpWorkflowDependencyInjectionValidationAnalyzer, 
+//     CodeFixes.CSharpWorkflowDependencyInjectionAnalyzerCodeFixProvider>;
+//
+// [TestClass]
+// public class WorkflowDependencyInjectionValidationCodeFixTests
+// {
+//     [TestMethod]
+//     public async Task ShouldRemainUnchangedIfNoWorkflowsFound()
+//     {
+//         var test =
+//             @"
+// using System;
+// using System.Runtime;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// public class Program
+// {
+// 	public void Run(string[] args)
+// 	{
+// 		var builder = Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+// 		{
+// 			services.AddDaprWorkflow(opt => { });
+// 		});
+//
+// 		using var host = builder.Build();
+// 		host.Start();
+// 	}
+// }
+//
+// public static class DaprExtensions
+// {
+//     public static IServiceCollection AddDaprWorkflow(this IServiceCollection serviceCollection, Action<WorkflowRuntimeOptions> configure) 
+//     {
+//         return serviceCollection;
+//     }    
+// }
+//
+// public sealed class WorkflowRuntimeOptions
+// {
+//     public void RegisterWorkflow<TWorkflow>() where TWorkflow : class, IWorkflow, new()
+//     {
+//     }
+// } 
+//
+// public interface IWorkflow {}
+//
+// public abstract class Workflow<TIn, TOut> : IWorkflow {}
+// ";
+//         var refAssemblies = ReferenceAssemblies.NetStandard.NetStandard20
+//             .AddPackages(ImmutableArray.Create(
+//                 new PackageIdentity("Microsoft.Extensions.Hosting", "8.0.0"),
+//                 new PackageIdentity("Microsoft.Extensions.DependencyInjection", "8.0.0")))
+//             .AddAssemblies(
+//                 ImmutableArray.Create(
+//                     "Microsoft.Extensions.DependencyInjection.Abstractions",
+//                     "Microsoft.Extensions.DependencyInjection.Abstractions",
+//                     "Microsoft.Extensions.Hosting",
+//                     "Microsoft.Extensions.Hosting.Abstractions",
+//                     "Microsoft.Extensions.Logging.Abstractions"));
+//
+//         await new VerifyCS.Test
+//         {
+//             ReferenceAssemblies = refAssemblies,
+//             TestState = {Sources = {test}},
+//             FixedCode = test,
+//             ExpectedDiagnostics = { }
+//         }.RunAsync();
+//     }
+//
+//     [TestMethod]
+//     public async Task ShouldRemainUnchangedIfWorkflowAlreadyRegistered()
+//     {
+//         var test =
+//             @"
+// using System;
+// using System.Runtime;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// public class Program
+// {
+// 	public void Run(string[] args)
+// 	{
+// 		var builder = Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+// 		{
+// 			services.AddDaprWorkflow(opt => 
+//             {
+//                 opt.RegisterWorkflow<MyWorkflow>(); 
+//             });
+// 		});
+//
+// 		using var host = builder.Build();
+// 		host.Start();
+// 	}
+// }
+//
+// public static class DaprExtensions
+// {
+//     public static IServiceCollection AddDaprWorkflow(this IServiceCollection serviceCollection, Action<WorkflowRuntimeOptions> configure) 
+//     {
+//         return serviceCollection;
+//     }    
+// }
+//
+// public sealed class WorkflowRuntimeOptions
+// {
+//     public void RegisterWorkflow<TWorkflow>() where TWorkflow : class, IWorkflow, new()
+//     {
+//     }
+// } 
+//
+// public interface IWorkflow {}
+//
+// public abstract class Workflow<TIn, TOut> : IWorkflow {}
+//
+// public sealed class MyWorkflow : Workflow<string, object> {};
+// ";
+//         var refAssemblies = ReferenceAssemblies.NetStandard.NetStandard20
+//             .AddPackages(ImmutableArray.Create(
+//                 new PackageIdentity("Microsoft.Extensions.Hosting", "8.0.0"),
+//                 new PackageIdentity("Microsoft.Extensions.DependencyInjection", "8.0.0")))
+//             .AddAssemblies(
+//                 ImmutableArray.Create(
+//                     "Microsoft.Extensions.DependencyInjection.Abstractions",
+//                     "Microsoft.Extensions.DependencyInjection.Abstractions",
+//                     "Microsoft.Extensions.Hosting",
+//                     "Microsoft.Extensions.Hosting.Abstractions",
+//                     "Microsoft.Extensions.Logging.Abstractions"));
+//
+//         await new VerifyCS.Test
+//         {
+//             ReferenceAssemblies = refAssemblies,
+//             TestState = {Sources = {test}},
+//             FixedCode = test,
+//             ExpectedDiagnostics = { }
+//         }.RunAsync();
+//     }
+//
+//     [TestMethod]
+//     public async Task ShouldCorrectMissingWorkflow()
+//     {
+//         var test = @"
+// using System;
+// using System.Runtime;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// public class Program
+// {
+// 	public void Run(string[] args)
+// 	{
+// 		var builder = Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+// 		{
+// 			services.AddDaprWorkflow(opt => 
+//             { 
+//             });
+// 		});
+//
+// 		using var host = builder.Build();
+// 		host.Start();
+// 	}
+// }
+//
+// public static class DaprExtensions
+// {
+//     public static IServiceCollection AddDaprWorkflow(this IServiceCollection serviceCollection, Action<WorkflowRuntimeOptions> configure) 
+//     {
+//         return serviceCollection;
+//     }    
+// }
+//
+// public sealed class WorkflowRuntimeOptions
+// {
+//     public void RegisterWorkflow<TWorkflow>() where TWorkflow : class, IWorkflow, new()
+//     {
+//     }
+// } 
+//
+// public interface IWorkflow {}
+//
+// public abstract class Workflow<TIn, TOut> : IWorkflow {}
+//
+// public sealed class MyWorkflow : Workflow<string, object> {};
+// ";
+//         
+//         var expected = @"
+// using System;
+// using System.Runtime;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// public class Program
+// {
+// 	public void Run(string[] args)
+// 	{
+// 		var builder = Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+// 		{
+// 			services.AddDaprWorkflow(opt => opt.RegisterWorkflow<MyWorkflow>());
+// 		});
+//
+// 		using var host = builder.Build();
+// 		host.Start();
+// 	}
+// }
+//
+// public static class DaprExtensions
+// {
+//     public static IServiceCollection AddDaprWorkflow(this IServiceCollection serviceCollection, Action<WorkflowRuntimeOptions> configure) 
+//     {
+//         return serviceCollection;
+//     }    
+// }
+//
+// public sealed class WorkflowRuntimeOptions
+// {
+//     public void RegisterWorkflow<TWorkflow>() where TWorkflow : class, IWorkflow, new()
+//     {
+//     }
+// } 
+//
+// public interface IWorkflow {}
+//
+// public abstract class Workflow<TIn, TOut> : IWorkflow {}
+//
+// public sealed class MyWorkflow : Workflow<string, object> {}";
+//         
+//         var expectedDiagnostic = VerifyCS.Diagnostic()
+//             .WithMessage(
+//                 $"The workflow type 'MyWorkflow' is not registered with the service provider")
+//             .WithSpan(42, 21, 42, 31) //Doesn't seem to like something about this line
+//             .WithArguments("MyWorkflow")
+//             .WithSeverity(DiagnosticSeverity.Warning);
+//         
+//         var refAssemblies = ReferenceAssemblies.NetStandard.NetStandard20
+//             .AddPackages(ImmutableArray.Create(
+//                 new PackageIdentity("Microsoft.Extensions.Hosting", "8.0.0"),
+//                 new PackageIdentity("Microsoft.Extensions.DependencyInjection", "8.0.0")))
+//             .AddAssemblies(
+//                 ImmutableArray.Create(
+//                     "Microsoft.Extensions.DependencyInjection.Abstractions",
+//                     "Microsoft.Extensions.DependencyInjection.Abstractions",
+//                     "Microsoft.Extensions.Hosting",
+//                     "Microsoft.Extensions.Hosting.Abstractions",
+//                     "Microsoft.Extensions.Logging.Abstractions"));
+//
+//         await new VerifyCS.Test
+//         {
+//             ReferenceAssemblies = refAssemblies,
+//             TestState = {Sources = {test}},
+//             FixedCode = expected,
+//             ExpectedDiagnostics = { expectedDiagnostic }
+//         }.RunAsync();
+//     }
+//
+//     [TestMethod]
+//     public async Task ShouldRegisterMultipleWorkflowsAsNecessary()
+//     {
+//         var test = @"
+// using System;
+// using System.Runtime;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// public class Program
+// {
+// 	public void Run(string[] args)
+// 	{
+// 		var builder = Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+// 		{
+// 			services.AddDaprWorkflow(opt => 
+//             { 
+//             });
+// 		});
+//
+// 		using var host = builder.Build();
+// 		host.Start();
+// 	}
+// }
+//
+// public static class DaprExtensions
+// {
+//     public static IServiceCollection AddDaprWorkflow(this IServiceCollection serviceCollection, Action<WorkflowRuntimeOptions> configure) 
+//     {
+//         return serviceCollection;
+//     }    
+// }
+//
+// public sealed class WorkflowRuntimeOptions
+// {
+//     public void RegisterWorkflow<TWorkflow>() where TWorkflow : class, IWorkflow, new()
+//     {
+//     }
+// } 
+//
+// public interface IWorkflow {}
+//
+// public abstract class Workflow<TIn, TOut> : IWorkflow {}
+//
+// public sealed class MyWorkflow : Workflow<string, object> {}
+// public sealed class MyOtherWorkflow : Workflow<bool, long> {}";
+//         
+//         var expected = @"
+// using System;
+// using System.Runtime;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// public class Program
+// {
+// 	public void Run(string[] args)
+// 	{
+// 		var builder = Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+// 		{
+// 			services.AddDaprWorkflow(opt => 
+//             {
+//                 opt.RegisterWorkflow<MyWorkflow>();
+//                 opt.RegisterWorkflow<MyOtherWorkflow>();
+//             });
+// 		});
+//
+// 		using var host = builder.Build();
+// 		host.Start();
+// 	}
+// }
+//
+// public static class DaprExtensions
+// {
+//     public static IServiceCollection AddDaprWorkflow(this IServiceCollection serviceCollection, Action<WorkflowRuntimeOptions> configure) 
+//     {
+//         return serviceCollection;
+//     }    
+// }
+//
+// public sealed class WorkflowRuntimeOptions
+// {
+//     public void RegisterWorkflow<TWorkflow>() where TWorkflow : class, IWorkflow, new()
+//     {
+//     }
+// } 
+//
+// public interface IWorkflow {}
+//
+// public abstract class Workflow<TIn, TOut> : IWorkflow {}
+//
+// public sealed class MyWorkflow : Workflow<string, object> {}
+// public sealed class MyOtherWorkflow : Workflow<bool, long> {}";
+//         
+//         var expectedDiagnostic = VerifyCS.Diagnostic()
+//             .WithMessage(
+//                 $"The workflow type 'MyWorkflow' is not registered with the service provider")
+//             .WithSpan(42, 21, 42, 31) //Doesn't seem to like something about this line
+//             .WithArguments("MyWorkflow")
+//             .WithSeverity(DiagnosticSeverity.Warning);
+//
+//         var expectedDiagnostic2 = VerifyCS.Diagnostic()
+//             .WithMessage(
+//                 $"The workflow type 'MyOtherWorkflow' is not registered with the service provider")
+//             .WithSpan(43, 21, 43, 36) //Doesn't seem to like something about this line
+//             .WithArguments("MyWorkflow")
+//             .WithSeverity(DiagnosticSeverity.Warning);
+//         
+//         var refAssemblies = ReferenceAssemblies.NetStandard.NetStandard20
+//             .AddPackages(ImmutableArray.Create(
+//                 new PackageIdentity("Microsoft.Extensions.Hosting", "8.0.0"),
+//                 new PackageIdentity("Microsoft.Extensions.DependencyInjection", "8.0.0")))
+//             .AddAssemblies(
+//                 ImmutableArray.Create(
+//                     "Microsoft.Extensions.DependencyInjection.Abstractions",
+//                     "Microsoft.Extensions.DependencyInjection.Abstractions",
+//                     "Microsoft.Extensions.Hosting",
+//                     "Microsoft.Extensions.Hosting.Abstractions",
+//                     "Microsoft.Extensions.Logging.Abstractions"));
+//
+//         await new VerifyCS.Test
+//         {
+//             ReferenceAssemblies = refAssemblies,
+//             TestState = {Sources = {test}},
+//             FixedCode = expected,
+//             ExpectedDiagnostics = { expectedDiagnostic, expectedDiagnostic2 }
+//         }.RunAsync();
+//     }
+// }
