4697d5c5fcc3e6d8204038c190cf56cf8106f123
Whit Waldo <whit.waldo@innovian.net>
2024-01-11 02:57:14 -0600

    Initial research
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ src/Dapr.Workflow.Analyzers/Analyzers/DAPR0500WorkflowInvocationsMustBeDeterministicAnalyzer.cs
@@ -0,0 +1,37 @@
+﻿//using System.Collections.Immutable;
+//using Microsoft.CodeAnalysis;
+//using Microsoft.CodeAnalysis.Diagnostics;
+
+//namespace Dapr.Workflow.Analyzers.Analyzers;
+
+///// <summary>
+///// Implements a Roslyn analyzer that validates that no types that implement IWorkflow call non-deterministic
+///// APIs except through types invoking IWorkflowActivity.
+///// </summary>
+//[DiagnosticAnalyzer(LanguageNames.CSharp)]
+//public sealed class DAPR0500WorkflowInvocationsMustBeDeterministic : DiagnosticAnalyzer
+//{
+//    /// <summary>
+//    /// The ID for diagnostics used by the <see cref="DAPR0500WorkflowInvocationsMustBeDeterministic"/> analyzer.
+//    /// </summary>
+//    public const string DiagnosticId = "DAPR0500";
+
+//    private const string HelpUri = "";
+
+//    private static readonly DiagnosticDescriptor Descriptor =
+//        new (DiagnosticId, "", "", "Usage", DiagnosticSeverity.Warning, true, "", HelpUri);
+
+//    /// <inheritdoc />
+//    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Descriptor);
+
+//    /// <inheritdoc />
+//    public override void Initialize(AnalysisContext context)
+//    {
+//        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+//        context.EnableConcurrentExecution();
+
+//        // https://github.com/dotnet/roslyn/blob/main/docs/compilers/Deterministic%20Inputs.md
+//        // https://github.com/Azure/azure-functions-durable-extension/tree/dev/src/WebJobs.Extensions.DurableTask.Analyzers
+//    }
+//}
+
