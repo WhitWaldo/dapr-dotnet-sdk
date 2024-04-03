d9fa60eadff771c9f59ad826cc73a93f26fc5b08
Whit Waldo <whit.waldo@innovian.net>
2024-04-03 02:35:57 -0500

    Updated comment for accuracy
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- src/Dapr.Workflow/WorkflowContext.cs
+++ src/Dapr.Workflow/WorkflowContext.cs
@@ -1,17 +1,4 @@
-﻿// ------------------------------------------------------------------------
-// Copyright 2022 The Dapr Authors
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// ------------------------------------------------------------------------
-
-namespace Dapr.Workflow
+﻿namespace Dapr.Workflow
 {
     using System;
     using System.Threading;
@@ -245,9 +232,9 @@ namespace Dapr.Workflow
         /// exception will be surfaced to the parent workflow, just like it is when an activity task fails with an
         /// exception. Child workflows also support automatic retry policies.
         /// </para><para>
-        /// Because child workflows are independent of their parents, terminating a parent workflow does not affect
-        /// any child workflows. You must terminate each child workflow independently using its instance ID, which
-        /// is specified by <see cref="ChildWorkflowTaskOptions.InstanceId" />.
+        /// Terminating a parent workflow terminates all the child workflows created by the workflow instance. See the documentation at
+        /// https://docs.dapr.io/developing-applications/building-blocks/workflow/workflow-features-concepts/#child-workflows regarding
+        /// the terminate workflow API for more information.
         /// </para>
         /// </remarks>
         /// <param name="workflowName">The name of the workflow to call.</param>
